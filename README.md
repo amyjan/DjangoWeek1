fb33681ded508659bca64cde7250c192fc6ffe44
amyjan <37925698+amyjan@users.noreply.github.com>
2018-10-04 01:46:52 +0800

    week3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Introduce myself
 嗨大家好，我是公衛系大四詹玟，這是我的DjangoClass的作業repo
 
-Week1
+Week1 + Week2  github/ heroku/ html/ css
 以下是我的heroku網站連結：https://protected-lake-78063.herokuapp.com/
--- helloworld/views.py
+++ helloworld/views.py
@@ -3,7 +3,16 @@ from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+import random
 
 
-def index(request):
-	return render(request, 'week2.html')
+#def index(request):
+#	return render(request, 'week2.html')
+
+
+def home (request):
+	base = "https://picsum.photos/200/200?image="
+	ran = random.sample(range(1,100),10)
+	images = [base+str(ran[i]) for i in range(len(ran))]
+	
+	return render(request,'week2.html',{'images':images})
