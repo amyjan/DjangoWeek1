573cfe0675f4b1b54bc05398c53cac8aba7c422a
amyjan <37925698+amyjan@users.noreply.github.com>
2018-10-18 21:14:18 +0800

    '1'

--- guestbook/views.py
+++ guestbook/views.py
@@ -29,6 +29,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
